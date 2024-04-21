451a155e715f7903b489a786f4db44bfbbf4000d
NEOLabs Software <101670923+NEOLabs-software@users.noreply.github.com>
2024-04-21 08:37:08 -0500

    HELP

--- dots/config/wireplumber/main.lua.d/51-alsa-rename.lua
+++
@@ -1,8 +0,0 @@
-table.insert(alsa_monitor.rules, {
-	matches = {{{"object.path", "equals", "alsa:pcm:1:front:1:playback"}}},
-	apply_properties = {["node.description"] = "Sink"},
-})
-table.insert(alsa_monitor.rules, {
-	matches = {{{"object.path", "equals", "alsa:pcm:1:front:1:capture"}}},
-	apply_properties = {["node.description"] = "Source"},
-})
